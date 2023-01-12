c3e543e9432de09acd3b1ecb8d512512fcb0478f
Minseok Ku <9msnine@naver.com>
2023-01-12 18:06:47 +0900

    MVC와 템플릿 엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,18 +4,27 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model){
+    public String hello(Model model) {
 
         model.addAttribute("data", "hello spring!!");
 
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+
+
+        model.addAttribute("data", name);
+
+        return "hello-template";
+    }
 
 
 }
