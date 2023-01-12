5ea4d74544601e9c52e3c92e443e0e6639f7633f
Minseok Ku <9msnine@naver.com>
2023-01-12 18:25:47 +0900

    @ResponseBody 객체 반환

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.hellospring.controller;
 
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,7 +38,51 @@ public class HelloController {
         //대신에 HTTP의 BODY에 문자 내용을 직접 반환(HTML BODY TAG를 말하는 것이 아님)
 
         return "hello " + name;
-
     }
+
+
+    //@ResponseBody 객체 반환
+    @ResponseBody
+    @GetMapping("hello-api")
+    public Hello helloApi(@RequestParam("name") String name) {
+
+        Hello hello = new Hello();
+
+        hello.setName(name);
+
+        //@ResponseBody 를 사용하고, 객체를 반환하면 객체가 JSON으로 변환됨
+
+        return hello;
+    }
+
+    static class Hello {
+
+        private String name;
+
+        public Hello() {
+
+        }
+
+        public Hello(String name) {
+            this.name = name;
+        }
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Hello{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
 
 }
