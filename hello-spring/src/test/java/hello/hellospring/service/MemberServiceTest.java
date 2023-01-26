7a91cfd3c8b3921de3f9cce2d9b7fc629614a686
Minseok Ku <9msnine@naver.com>
2023-01-26 17:41:59 +0900

    memberservice test

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,20 +1,69 @@
 package hello.hellospring.service;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
     @Test
-    void join() {
+    void 회원가입() {
+
+        //given
+        Member member = new Member();
+        member.setName("spring");
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member result = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(result.getName());
+    }
+
+
+    @Test
+    public void dupCheck() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+
+        memberService.join(member1);
+
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+
     }
 
     @Test
     void findMembers() {
+        //given
+
+        //when
+
+        //then
     }
 
     @Test
     void findOne() {
+        //given
+
+        //when
+
+        //then
     }
 }
