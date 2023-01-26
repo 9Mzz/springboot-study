305319b8bd24836b19d4df92abdb4f479daeee57
Minseok Ku <9msnine@naver.com>
2023-01-26 18:03:15 +0900

    test-spring re

--- test-spring/src/main/java/test/testspring/TestSpringApplication.java
+++ test-spring/src/main/java/test/testspring/TestSpringApplication.java
@@ -1,4 +1,4 @@
-package etst.testspring;
+package test.testspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- test-spring/src/test/java/test/testspring/TestSpringApplicationTests.java
+++ test-spring/src/test/java/test/testspring/TestSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package etst.testspring;
+package test.testspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
