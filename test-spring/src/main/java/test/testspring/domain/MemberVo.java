41516b4473ef6fa48eadc9c41dc3b998b21f50da
Minseok Ku <9msnine@naver.com>
2023-01-27 10:06:52 +0900

    test re end

---
+++ test-spring/src/main/java/test/testspring/domain/MemberVo.java
@@ -0,0 +1,11 @@
+package test.testspring.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberVo {
+
+    private Long   id;
+    private String name;
+
+}
---
+++ test-spring/src/main/java/test/testspring/service/MemberService.java
@@ -0,0 +1,44 @@
+package test.testspring.service;
+
+import org.springframework.stereotype.Service;
+import test.testspring.domain.MemberVo;
+import test.testspring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository repository;
+
+    public MemberService(MemberRepository repository) {
+        this.repository = repository;
+    }
+
+    public Long join(MemberVo memberVo) {
+
+        memberDuplicateCheck(memberVo);
+
+        repository.save(memberVo);
+
+        return memberVo.getId();
+    }
+
+    private void memberDuplicateCheck(MemberVo memberVo) {
+        repository.findbyName(memberVo.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<MemberVo> findAll() {
+
+        return repository.findAll();
+    }
+
+    public Optional<MemberVo> findOne(Long memberId) {
+
+
+        return repository.findbyId(memberId);
+    }
+}
