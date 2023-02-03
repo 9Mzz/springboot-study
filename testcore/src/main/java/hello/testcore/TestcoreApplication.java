5c193c616608ca24c3d04579b06d8d3453bda8eb
Minseok Ku <9msnine@naver.com>
2023-02-03 19:58:05 +0900

    testcore add

---
+++ testcore/src/main/java/hello/testcore/TestcoreApplication.java
@@ -0,0 +1,13 @@
+package hello.testcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcoreApplication.class, args);
+    }
+
+}
---
+++ testcore/src/test/java/hello/testcore/TestcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.testcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
