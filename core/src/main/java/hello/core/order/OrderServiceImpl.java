d77d7e125b6fe2260198d6c5c5284eb3e3270f01
Minseok Ku <9msnine@naver.com>
2023-02-06 12:11:14 +0900

    DI Add

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,20 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository repository     = new MemoryMemberRepository();
-    private final DiscountPolicy   discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy   discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy   = discountPolicy;
+    }
+// private final DiscountPolicy   discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy   discountPolicy = new RateDiscountPolicy();
 
     /**
      * 주문 생성
@@ -22,7 +27,7 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
 
-        Member member        = repository.findById(memberId);
+        Member member        = memberRepository.findById(memberId);
         int    discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,45 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void discount() {
+
+        //given
+        Member testA = new Member(1L, "testA", Grade.VIP);
+
+        //when
+        int result = discountPolicy.discount(testA, 13000);
+
+        System.out.println("discount = " + result + "원 할인됨.");
+        //then
+        assertThat(result).isEqualTo(1300);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+    void discountFail() {
+
+        //given
+        Member testA = new Member(2L, "testA", Grade.GRADE);
+
+        //when
+        int result = discountPolicy.discount(testA, 10000);
+
+        System.out.println("discountFail = " + result + "원 할인됨.");
+        //then
+        assertThat(result).isEqualTo(0);
+
+    }
+}
