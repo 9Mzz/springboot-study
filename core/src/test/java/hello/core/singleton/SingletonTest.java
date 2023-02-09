8f9a582f7e7a1ad49d8768216c6241abc7c2cd60
Minseok Ku <9msnine@naver.com>
2023-02-09 16:45:06 +0900

    singleton test correction

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -61,8 +61,11 @@ public class SingletonTest {
 
         //2. 조회: 호출할 때 마다 객체를 생성
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
-        //참조값이 다른 것을 확인
+        
+        //참조값이 같은 것을 확인
+        //memberService1 = hello.core.member.MemberServiceImpl@77a7cf58
         System.out.println("memberService1 = " + memberService1);
+        //memberService2 = hello.core.member.MemberServiceImpl@77a7cf58
         System.out.println("memberService2 = " + memberService2);
 
         //memberService1 == memberService2
