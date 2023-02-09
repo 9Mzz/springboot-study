d2da047bf77a8de32e1b30576436fd8102207e85
Minseok Ku <9msnine@naver.com>
2023-02-09 16:43:50 +0900

    Singleton test

--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -5,12 +5,10 @@ public class SingletonService {
     private static final SingletonService instance = new SingletonService();
 
     public static SingletonService getInstance() {
-
         return instance;
     }
 
     private SingletonService() {
-
     }
 
     public void logic() {
