f41d766f18b57d3d2b58c99362921f86bce69bea
Minseok Ku <9msnine@naver.com>
2023-03-06 14:16:22 +0900

    Appconfig crash

--- coretest/src/main/java/test/core/AutoAppConfig.java
+++ coretest/src/main/java/test/core/AutoAppConfig.java
@@ -1,15 +1,13 @@
 package test.core;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import test.core.member.MemberRepository;
 import test.core.member.MemberRepositoryImpl;
 
-@Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
+@Configuration @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) public class AutoAppConfig {
+
     public MemberRepository memberRepository() {
         return new MemberRepositoryImpl();
     }
