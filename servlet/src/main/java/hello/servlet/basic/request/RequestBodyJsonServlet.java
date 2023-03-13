5453dd9efbfa464cbf2608f01182fa5e0cd98a0d
Minseok Ku <9msnine@naver.com>
2023-03-13 16:43:20 +0900

    API messege body - JSON

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,39 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletInputStream inputStream = request.getInputStream();
+        String             messegebody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messegebody = " + messegebody);
+
+        //body를 담고, VO Clas를 담는다.
+        HelloData helloData = objectMapper.readValue(messegebody, HelloData.class);
+        System.out.println("helloData = " + helloData.getUserName());
+        System.out.println("helloData = " + helloData.getAge());
+
+        response.getWriter()
+                .write("ok");
+
+
+    }
+}
