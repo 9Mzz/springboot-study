3164e6399beddd7c249b78fbd16a2e26e826a469
Minseok Ku <9msnine@naver.com>
2023-03-13 13:06:16 +0900

    Get query parameter

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -1,4 +1,7 @@
-package hello.servlet.basic;
+package hello.servlet.basic.request;
+
+import org.apache.coyote.Response;
+import org.springframework.core.ParameterNameDiscoverer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /**
  * 1. 파라미터 전송 기능
@@ -14,38 +18,43 @@ import java.io.IOException;
  * 2. 동일한 파라미터 전송 가능
  * http://localhost:8080/request-param?username=hello&username=kim&age=20
  */
-
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param") public class RequestParamServlet extends HttpServlet {
 
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //        System.out.println("RequestParamServlet.service");
+
+        System.out.println("RequestParamServlet.service");
 
         System.out.println("[전체 파라미터 조회] - start");
-
+        /**
+         * paramName = Key
+         * request.getParameter(paramName)) = value
+         */
         request.getParameterNames()
                .asIterator()
-               .forEachRemaining(paramName -> System.out.println("paramName = " + paramName));
+               .forEachRemaining(paramName -> System.out.println(paramName + " =  " + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
 
-
         System.out.println("[단일 파라미터 조회]");
         String username = request.getParameter("username");
-        String age = request.getParameter("age");
+        String age      = request.getParameter("age");
         System.out.println("username = " + username + ", age = " + age);
-        System.out.println("[단일 파라미터 조회]");
         System.out.println();
 
-
+        /**
+         * http://localhost:8088/request-param?username=hello&age=20&username=hello2
+         */
         System.out.println("[이름이 같은 복수 파라미터 조회]");
         String[] usernames = request.getParameterValues("username");
         for (String userName : usernames) {
             System.out.println("userName = " + userName);
         }
+
         response.getWriter()
                 .write("ok");
 
     }
+
 }
