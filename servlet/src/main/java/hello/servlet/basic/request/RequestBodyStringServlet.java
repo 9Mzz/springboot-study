5943393bd3b653e05c080b938c9cec7df66b238a
Minseok Ku <9msnine@naver.com>
2023-03-13 14:13:32 +0900

    API Messege body = text

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "RequestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //body의 내용을 바이트코드로 얻을 수 있다.
+        ServletInputStream inputStream = request.getInputStream();
+
+        String messegebody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messegebody = " + messegebody);
+
+        response.getWriter()
+                .write("ok");
+
+
+    }
+}
