eeda2c1165819b76da1cd6a80615c0ef0efa94eb
Minseok Ku <9msnine@naver.com>
2023-03-16 13:11:29 +0900

    V5-v3

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -6,8 +6,12 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import java.util.Map;
 
 public class MemberFormControllerV3 implements ControllerV3 {
+
+
     @Override
     public ModelView process(Map<String, String> paramMap) {
+        System.out.println("MemberFormControllerV3.process");
+
         return new ModelView("new-form");
     }
 }
