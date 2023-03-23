c8218ef5983f3be43237d744eb841e9283284ec5
Minseok Ku <9msnine@naver.com>
2023-03-23 11:31:26 +0900

    springmvc test

---
+++ springmvctest/src/main/java/hello/springmvctest/basic/LogTestController.java
@@ -0,0 +1,28 @@
+package hello.springmvctest.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LogTestController {
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring!!";
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+
+        log.info("error test = " + name);
+
+
+        return name;
+    }
+
+}
