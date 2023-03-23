0a0696ef7a9310d48ee335f5bd0cbb46cfede46d
Minseok Ku <9msnine@naver.com>
2023-03-23 14:44:58 +0900

    domain add

--- item-service/src/main/java/hello/itemservice/domain/Item.java
+++ item-service/src/main/java/hello/itemservice/domain/Item.java
@@ -1,13 +1,13 @@
 package hello.itemservice.domain;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Data //는 위험하다
-//@Setter
-//@ToString
+//@Data //는 위험하다
+@Getter
+@Setter
+@ToString
 public class Item {
 
     // int 대신 Integer 쓴 이유 : 값이 없을 수도 있어서
--- item-service/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ item-service/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,21 +41,34 @@ class ItemRepositoryTest {
 
         itemRepository.save(item1);
         itemRepository.save(item2);
-        //when
 
+        //when
+        List<Item> result = itemRepository.findAll();
 
 
         //then
-
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
     }
 
     @Test
     void update() {
         //given
+        Item item1    = new Item("itemA", 10000, 10);
+        Item saveItem = itemRepository.save(item1);
+        Long itemId   = saveItem.getId();
 
         //when
+        Item updateParam = new Item("itemB", 20000, 30);
+        itemRepository.update(itemId, updateParam);
 
         //then
+        Item item = itemRepository.findById(itemId);
+
+        assertThat(item.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(item.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(item.getQuanity()).isEqualTo(updateParam.getQuanity());
+
 
     }
 
