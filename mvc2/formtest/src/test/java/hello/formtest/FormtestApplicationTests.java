590bc4166dd6171097c115fc98ac3aed822ae615
Minseok Ku <9msnine@naver.com>
2023-05-24 11:17:39 +0900

    복습

---
+++ mvc2/formtest/src/test/java/hello/formtest/FormtestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.formtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
