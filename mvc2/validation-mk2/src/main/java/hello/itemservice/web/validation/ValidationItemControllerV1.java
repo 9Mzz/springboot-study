cc1e70c33339a8e433e71014eaa43e3cf8760b2a
Minseok Ku <9msnine@naver.com>
2023-06-20 22:52:07 +0900

    validation v2

--- mvc2/validation-mk2/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ mvc2/validation-mk2/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -2,24 +2,17 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
-import jdk.jshell.ErroneousSnippet;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
-@Slf4j
 public class ValidationItemControllerV1 {
 
     private final ItemRepository itemRepository;
@@ -45,35 +38,8 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(Model model, @ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-
-        //오류 상황 추가
-        Map<String, String> errors = new HashMap<>();
-
-        if(!StringUtils.hasText(item.getItemName())) {
-            errors.put("itemName", "상품명: 필수, 공백X");
-        }
-        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            errors.put("price", "가격: 1000원 이상, 1백만원 이하");
-        }
-        if(item.getQuantity() == null || item.getQuantity() > 10000) {
-            errors.put("quantity", "수량: 최대 9999");
-        }
-        if(item.getPrice() != null && item.getQuantity() != null) {
-            int result = item.getPrice() * item.getQuantity();
-            if(result < 10000) {
-                errors.put("globalErrors", "가격 * 수량의 합은 10,000원 이상, 현재 가격 = " + result);
-            }
-        }
-        if(!errors.isEmpty()) {
-            log.info("error code = {}", errors);
-            model.addAttribute("errors", errors);
-            return "validation/v1/addForm";
-        }
-
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         Item savedItem = itemRepository.save(item);
-
-
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v1/items/{itemId}";
