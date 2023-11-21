0301ee4f4875e4b74bc6be239a743b8fa3a7191b
Minseok Ku <9msnine@naver.com>
2023-11-21 17:06:30 +0900

    re

--- mvc2/login-mk3/src/main/java/hello/login/web/session/SessionManager.java
+++ mvc2/login-mk3/src/main/java/hello/login/web/session/SessionManager.java
@@ -1,9 +1,8 @@
 package hello.login.web.session;
 
-import java.lang.ref.ReferenceQueue;
+
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.http.Cookie;
@@ -17,51 +16,43 @@ public class SessionManager {
   public static final String              MY_SESSION_ID = "mySessionId";
   private final       Map<String, Object> sessionStore  = new ConcurrentHashMap<>();
 
-
   public void createSession(Object value, HttpServletResponse response) {
-
-    //랜덤uid 생성
     String sessionId = UUID.randomUUID()
         .toString();
-    //map에 저장
     sessionStore.put(sessionId, value);
-    //response 저장
     Cookie mySessionCookie = new Cookie(MY_SESSION_ID, sessionId);
     response.addCookie(mySessionCookie);
   }
 
-  /**
-   * 세션 조회
-   */
-  public Object getSession(HttpServletRequest request) {
 
-    Cookie mySessionCookie = getCookie(request, MY_SESSION_ID);
+  public Object getCookie(HttpServletRequest request) {
+    Cookie mySessionCookie = getSession(request, MY_SESSION_ID);
     if (mySessionCookie == null) {
       return null;
     }
     return sessionStore.get(mySessionCookie.getValue());
   }
 
-  /**
-   * 세션 제거
-   */
   public void expire(HttpServletRequest request) {
-    Cookie mySessionCookie = getCookie(request, MY_SESSION_ID);
+    Cookie mySessionCookie = getSession(request, MY_SESSION_ID);
     if (mySessionCookie != null) {
       sessionStore.remove(mySessionCookie.getValue());
     }
   }
 
-  public Cookie getCookie(HttpServletRequest request, String cookieName) {
-    if (request.getCookies() == null) {
+
+  public Cookie getSession(HttpServletRequest request, String cookieName) {
+    Cookie[] requestCookies = request.getCookies();
+    if (requestCookies == null) {
       return null;
     }
 
-    return Arrays.stream(request.getCookies())
+    return Arrays.stream(requestCookies)
         .filter(cookie -> cookie.getName()
             .equals(cookieName))
         .findFirst()
         .orElse(null);
+
   }
 
 
