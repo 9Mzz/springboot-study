13366486871b3f1e07a5c8219672cf60cbb63f94
Minseok Ku <9msnine@naver.com>
2023-11-24 11:09:08 +0900

    toy end

--- mvc2/login-toy2/src/main/java/hello/login/web/HomeController.java
+++ mvc2/login-toy2/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,35 @@
 package hello.login.web;
 
+import hello.login.web.member.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.MethodOverride;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/items";
+  //  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
+
+  @GetMapping("/")
+  public String sessionHome(
+      @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+      Model model) {
+
+    if (loginMember == null) {
+      return "home";
     }
+
+    model.addAttribute("member", loginMember);
+
+    return "loginHome";
+  }
 }
---
+++ mvc2/login-toy2/src/main/java/hello/login/web/member/Member.java
@@ -0,0 +1,18 @@
+package hello.login.web.member;
+
+
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class Member {
+
+  private Long   id;
+  @NotBlank
+  private String loginId;
+  @NotBlank
+  private String password;
+  @NotBlank
+  private String name;
+
+}
