e1b7f8f316c77d3efb5a7379adf27c90eee2e863
Minseok Ku <9msnine@naver.com>
2023-12-20 12:34:09 +0900

    API 예외 처리 re

--- mvc2/exception/src/main/java/hello/exception/exception/api/ApiExceptionController.java
+++ mvc2/exception/src/main/java/hello/exception/exception/api/ApiExceptionController.java
@@ -1,9 +1,10 @@
 package hello.exception.exception.api;
 
+import jdk.jfr.DataAmount;
+import jdk.jfr.Name;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnThreading;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +16,12 @@ public class ApiExceptionController {
 
   @GetMapping("/api/members/{id}")
   public MemberDto getMember(@PathVariable("id") String id) {
+    log.info("id = {}", id);
     if (id.equals("ex")) {
-      throw new RuntimeException("잘못된 사용자");
+      throw new RuntimeException("런타임 오류 발생, 잘못된 사용자");
     }
 
-    return new MemberDto(id, "hello" + id);
+    return new MemberDto(id, "hello " + id);
   }
 
 
