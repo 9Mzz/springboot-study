e969837ad813e0a7d0f247e1b1211c135828b50b
Minseok Ku <9msnine@naver.com>
2024-01-08 09:51:08 +0900

    파일 업로드 re

---
+++ mvc2/upload01/src/test/java/hello/upload01/Upload01ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.upload01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upload01ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
