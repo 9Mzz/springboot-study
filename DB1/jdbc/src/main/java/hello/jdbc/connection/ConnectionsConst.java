4963423b0ce0d8186895cdc45d1fe0fe2570d6c8
Minseok Ku <9msnine@naver.com>
2024-01-15 01:22:49 +0900

    데이터베이스 연결

---
+++ DB1/jdbc/src/main/java/hello/jdbc/connection/ConnectionsConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+public interface ConnectionsConst {
+    String URL = "jdbc:h2:tcp://localhost/~/test";
+    String USERNAME = "sa";
+    String PASSWORD = "";
+
+
+}
