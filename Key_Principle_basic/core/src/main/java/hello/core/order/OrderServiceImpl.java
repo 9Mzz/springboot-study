8086be2f54c9b4bf723e414cef0832cc6df046a5
Minseok Ku <9msnine@naver.com>
2024-01-30 02:57:12 +0900

    코드 설정

--- Key_Principle_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Key_Principle_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderSerivce {
 
     private final MemberRepository repository     = new MemoryMemberRepository();
-    private final DiscountPolicy   discountPolicy = new FixDiscountPolicy();
+    //고정 할인
+    //    private final DiscountPolicy   discountPolicy = new FixDiscountPolicy();
+    // % 할인
+    private final DiscountPolicy   discountPolicy = new RateDiscountPolicy();
 
 
     @Override
