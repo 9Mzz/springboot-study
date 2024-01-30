d9523c4e5cdeaa339eabe41ee7b0f77a08525318
Minseok Ku <9msnine@naver.com>
2024-01-31 01:24:08 +0900

    @Configuration과 싱글톤

--- Key_Principle_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Key_Principle_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,20 +6,25 @@ import hello.core.member.MemberRepository;
 
 public class OrderServiceImpl implements OrderSerivce {
 
-    private final MemberRepository repository;
-    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy   discountPolicy;
 
-    public OrderServiceImpl(MemberRepository repository, DiscountPolicy discountPolicy) {
-        this.repository = repository;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        Member member        = repository.findById(memberId);
+        Member member        = memberRepository.findById(memberId);
         int    discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
