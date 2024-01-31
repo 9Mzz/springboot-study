385939157262533001ec5371da53331cbe044eb1
Minseok Ku <9msnine@naver.com>
2024-01-31 20:15:52 +0900

    탐색 위치와 기본 스캔 대상

--- Key_Principle_basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ Key_Principle_basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,10 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //@component 어노테이션이 붙은 클래스들을 찾아서 전부 등록해줌.
 //(type = FilterType.ANNOTATION, classes = Configuration.class)) 하는 이유 : @configuration은 수동등록인데 자동등록하면 안됨
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 public class AutoAppConfig {
 
 
