fcc7c5b437dbdf5da96a3cb0557cbc696d8f0d5e
Minseok Ku <9msnine@naver.com>
2024-01-31 23:49:30 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- Key_Principle_basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ Key_Principle_basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.FilterType;
 //@component 어노테이션이 붙은 클래스들을 찾아서 전부 등록해줌.
 //(type = FilterType.ANNOTATION, classes = Configuration.class)) 하는 이유 : @configuration은 수동등록인데 자동등록하면 안됨
 @ComponentScan(
-        basePackages = "hello.core.member",
+        basePackages = "hello.core.*",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
