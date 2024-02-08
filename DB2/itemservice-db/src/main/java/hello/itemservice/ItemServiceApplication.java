6c0dd70cc3ccdcb98f5cab50c3b136bf98248e21
Minseok Ku <9msnine@naver.com>
2024-02-08 20:59:30 +0900

    JdbcTemplate - SimpleJdbcInsert

--- DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV2Config;
+import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +9,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
+//@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 //@Import(MemoryConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
