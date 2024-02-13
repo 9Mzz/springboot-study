6666274ea1b43543d8de16fbb747f3f94b1118ee
Minseok Ku <9msnine@naver.com>
2024-02-13 20:55:26 +0900

    Mybatis 롤백

--- DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ DB2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.MyBatisConfig;
+import hello.itemservice.config.MemoryConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -14,8 +14,7 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
-//@Import(MemoryConfig.class)
-@Import(MyBatisConfig.class)
+@Import(MemoryConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- DB2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ DB2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
+@Commit
 @Transactional
 @SpringBootTest
 class ItemRepositoryTest {
