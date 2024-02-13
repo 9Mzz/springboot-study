23eb4d26caeaea491eb3d519753379698266f5cb
Minseok Ku <9msnine@naver.com>
2024-02-13 20:51:31 +0900

    실패

--- DB2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ DB2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -5,8 +5,9 @@ import lombok.Data;
 @Data
 public class Item {
 
-    private Long    id;
-    private String  itemName;
+    private Long id;
+
+    private String itemName;
     private Integer price;
     private Integer quantity;
 
