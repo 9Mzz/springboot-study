56548f5fb719af8c060349060b2673bf1aa600d5
Minseok Ku <9msnine@naver.com>
2024-02-16 02:29:46 +0900

    java.lang.NoClassDefFoundError: javax/persistence/Entity 오류 발생 - 해결x

--- DB2/itemservice/src/main/java/hello/itemservice/domain/Item.java
+++ DB2/itemservice/src/main/java/hello/itemservice/domain/Item.java
@@ -7,19 +7,18 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class Item {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long    id;
-    @Column(name = "item_name")
-    private String  itemName;
-    @Column(name = "price")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "item_name", length = 10)
+    private String itemName;
     private Integer price;
-    @Column(name = "quantity")
     private Integer quantity;
+
+    public Item() {
+    }
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
