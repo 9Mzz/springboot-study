8676a960826a3c57b8400ff75dbabc16d4574bf1
Minseok Ku <9msnine@naver.com>
2024-02-15 11:48:24 +0900

    JPA 적용 1 - 개발

--- DB2/itemservice/src/main/java/hello/itemservice/domain/Item.java
+++ DB2/itemservice/src/main/java/hello/itemservice/domain/Item.java
@@ -1,17 +1,24 @@
 package hello.itemservice.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long    id;
+    @Column(name = "item_name")
     private String  itemName;
+    @Column(name = "price")
     private Integer price;
+    @Column(name = "quantity")
     private Integer quantity;
 
     public Item(String itemName, Integer price, Integer quantity) {
