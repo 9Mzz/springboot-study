7f74ef731af708e6b1ec4d1464c637f602ab3ba7
05016 <9msnine@naver.com>
2024-03-06 22:09:35 +0900

    고급 매핑

---
+++ JPABook/ch08-jpa-start8/src/main/java/jpabook/start/Member.java
@@ -0,0 +1,84 @@
+package jpabook.start;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: HolyEyE Date: 13. 5. 24. Time: 오후 7:43
+ */
+@Entity
+//@org.hibernate.annotations.DynamicUpdate
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long    id;
+    @Column(name = "user_name")
+    private String  username;
+    private Integer age;
+    // n
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
+    //con
+    public Member() {
+    }
+
+    public Member(String username, Integer age) {
+        this.username = username;
+        this.age      = age;
+    }
+
+    public Member(Long id, String username, Integer age) {
+        this.id       = id;
+        this.username = username;
+        this.age      = age;
+    }
+
+    //g&s
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+    //
+
+
+    @Override
+    public String toString() {
+        return "Member{" + "id=" + id + ", username='" + username + '\'' + ", age=" + age + ", team=" + team + '}';
+    }
+}
---
+++ JPABook/ch08-jpa-start8/src/main/java/jpabook/start/MemberMain.java
@@ -0,0 +1,81 @@
+package jpabook.start;
+
+import javax.persistence.*;
+
+
+public class MemberMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager        em  = emf.createEntityManager();
+        EntityTransaction    tx  = em.getTransaction();
+
+        try {
+            tx.begin();
+            //bm
+            userSave(em);
+            //            printUser(em);
+            printUserAndTeam(em);
+            //            printReference(em);
+            //            findEquals(em);
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+
+    private static void userSave(EntityManager em) {
+
+        Team teamA = new Team();
+        teamA.setName("팀 A");
+        em.persist(teamA);
+
+        Member memberA = new Member();
+        memberA.setUsername("회원 1");
+        memberA.setAge(52);
+        memberA.setTeam(teamA);
+        em.persist(memberA);
+
+
+        //DB에 변동값 저장 후 1차캐시 끊기
+        em.flush();
+        em.clear();
+
+
+    }
+
+    private static void printUser(EntityManager em) {
+        System.out.println("MemberMain.printUser");
+        Member findMember = em.find(Member.class, 1L);
+        //        System.out.println("[printUser] 회원 이름 = " + findMember.getUsername());
+    }
+
+    private static void printUserAndTeam(EntityManager em) {
+        Member findMember = em.find(Member.class, 1L);
+        Team   findTeam   = findMember.getTeam();
+        System.out.println("[printUserAndTeam] 회원 이름 :  = " + findMember.getUsername());
+        System.out.println("[printUserAndTeam] 소속팀 = " + findTeam.getName());
+    }
+
+    private static void printReference(EntityManager em) {
+        Member findMember = em.getReference(Member.class, 1L);
+        //                Team   findTeam   = findMember.getTeam();
+        //        System.out.println("[before] findMember = " + findMember.getClass());
+        //        System.out.println("findMember = " + findMember);
+        //                System.out.println("findTeam = " + findTeam);
+        //        System.out.println("[after] findMember = " + findMember.getClass());
+    }
+
+    private static void findEquals(EntityManager em) {
+        Member memberA = em.find(Member.class, 1L);
+        Member memberB = em.find(Member.class, 1L);
+        System.out.println(memberA.getClass() == memberB.getClass());
+
+    }
+}
