1fc90d789763ec144f44a9d9d0ff704b8e256d33
05016 <9msnine@naver.com>
2024-03-22 03:49:53 +0900

    querydsl start

---
+++ JPABook/ch10-gradle/src/main/java/hello/ch10gradle/Ch10GradleApplication.java
@@ -0,0 +1,13 @@
+package hello.ch10gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch10GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch10GradleApplication.class, args);
+    }
+
+}
