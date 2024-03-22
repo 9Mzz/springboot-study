7db5d0884caf481747b1932173c77789b8deb72a
05016 <9msnine@naver.com>
2024-03-22 14:37:23 +0900

    db2 delete

--- DB2/jpa/src/main/java/hello/jpa/repository/SpringDataJpaItemRepository.java
+++
@@ -1,7 +0,0 @@
-package hello.jpa.repository;
-
-import hello.jpa.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataJpaItemRepository extends JpaRepository<Member, Long> {
-}
