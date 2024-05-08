ab8ec3b5c1f39bd9656ab4d48d0d146fb3823115
05016 <9msnine@naver.com>
2024-05-08 14:43:19 +0900

    datajpa

--- JPA/dataJPA/src/main/java/hello/datajpa/DataJpaApplication.java
+++ JPA/dataJPA/src/main/java/hello/datajpa/DataJpaApplication.java
@@ -1,13 +1,9 @@
 package hello.datajpa;
 
-import hello.datajpa.config.SpringDataConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-
-@Import(SpringDataConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.datajpa.web")
+@SpringBootApplication
 public class DataJpaApplication {
 
     public static void main(String[] args) {
