8a191c45811bf42c0010c973b1c799ad0aa64577
05016 <9msnine@naver.com>
2024-06-18 01:10:47 +0900

    queryDSL - 중급 문법 2

--- JPA/queryDSL/src/test/java/hello/querydsl/QueryDSLMiddleTestV2.java
+++ JPA/queryDSL/src/test/java/hello/querydsl/QueryDSLMiddleTestV2.java
@@ -1,5 +1,8 @@
 package hello.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.querydsl.domain.Member;
 import hello.querydsl.domain.Team;
@@ -10,6 +13,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import java.util.List;
+
+import static hello.querydsl.domain.QMember.member;
 
 @Slf4j
 @SpringBootTest
@@ -40,10 +48,112 @@ public class QueryDSLMiddleTestV2 {
         em.persist(memberE);
     }
 
-    // 동적 쿼리 - BooleanBuilder 사용
+    // 4 동적 쿼리 - BooleanBuilder 사용
     @Test
     void dynamicQuery_BooleanBuilder() {
+        String  userNameParam = "memberA";
+        Integer ageParam      = 10;
 
+        List<Member> searchMemberV1 = searchMemberV1(userNameParam, ageParam);
+        for (Member member1 : searchMemberV1) {
+            log.info("member = {}", member1);
+        }
+
+    }
+
+    private List<Member> searchMemberV1(String userNameParam, Integer ageParam) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (userNameParam != null) {
+            builder.and(member.userName.eq(userNameParam));
+        }
+        if (ageParam != null) {
+            builder.and(member.age.eq(ageParam));
+        }
+        return query.select(member)
+                .from(member)
+                .where(builder)
+                .fetch();
+    }
+
+    // 5 동적 쿼리 - Where 다중 파라미터 사용
+    @Test
+    void dynamicQuery_WhereParam() {
+        String  userNameParam = "membeerA";
+        Integer ageParam      = 10;
+
+        List<Member> searchMemberV2 = searchMemberV2(userNameParam, ageParam);
+        for (Member member : searchMemberV2) {
+            log.info("member = {}", member);
+        }
+
+    }
+
+    private List<Member> searchMemberV2(String userNameCond, Integer ageCond) {
+        return query.select(member)
+                .from(member)
+                .where(userNameCond(userNameCond), ageCond(ageCond))
+                .fetch();
+    }
+
+    private BooleanExpression userNameCond(String userNameCond) {
+        return userNameCond != null ? member.userName.eq(userNameCond) : null;
+    }
+
+    private BooleanExpression ageCond(Integer ageCond) {
+        return ageCond != null ? member.age.eq(ageCond) : null;
+    }
+
+    // 6 수정, 삭제 벌크 연산
+    @Test
+    @Commit
+    void buliUpdate() {
+        long userNameCount = query.update(member)
+                .set(member.userName, "비회원")
+                .where(member.age.goe(5))
+                .execute();
+        long ageAddCount = query.update(member)
+                .set(member.age, member.age.add(1))
+                .execute();
+        long ageDeleteCount = query.update(member)
+                .set(member.age, member.age.add(-1))
+                .execute();
+        long ageDoubleCount = query.update(member)
+                .set(member.age, member.age.multiply(2))
+                .execute();
+        em.flush();
+        em.clear();
+
+        log.info("userNameCount = {}", userNameCount);
+        log.info("ageAddCount = {}", ageAddCount);
+        log.info("ageDeleteCount = {}", ageDeleteCount);
+        log.info("ageDoubleCount = {}", ageDoubleCount);
+
+        List<Member> result = query.selectFrom(member)
+                .fetch();
+        for (Member member : result) {
+            log.info("member = {}", member);
+        }
+    }
+
+    // 쿼리 한번으로 대량 데이터 삭제
+    @Test
+    void bulkDelete() {
+        long deleteCount = query.delete(member)
+                .where(member.age.loe(18))
+                .execute();
+        log.info("deleteCount = {}", deleteCount);
+    }
+
+    // 7 SQL function 호출하기
+    @Test
+    void sqlFunction() {
+        List<String> result = query.select(Expressions.stringTemplate("function('replace', {0}, {1}, {2})"
+                        , member.userName, "member", "M"))
+                .from(member)
+                .fetch();
+        for (String s : result) {
+            log.info("s = {}", s);
+        }
     }
 
 }
