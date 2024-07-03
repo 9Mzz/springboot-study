70133e45e9a791de5bd3892331d5f2fb69060122
05016 <nico1a@naver.com>
2024-07-03 11:39:36 +0900

    스프링 트랜잭션 전파7 - REQUIRES_NEW

--- DB2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ DB2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -133,7 +134,29 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 Commit");
         Assertions.assertThatThrownBy(() -> txManager.commit(outer))
                 .isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        inner_Logic();
+
+        log.info("외부 트랜잭션 Commit");
+        txManager.commit(outer);
 
     }
 
+    private void inner_Logic() {
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("outer.isNewTransaction() = {}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 Rollback");
+        txManager.rollback(inner);
+    }
+    
 }
