3b93461935016a13f7e9df9ffea0e6295dbf866e
05016 <nico1a@naver.com>
2024-07-16 02:39:39 +0900

    로그 추적기 V1 - 프로토타입 개발

--- advanced/advanced/src/test/java/hello/advanced/AdvancedApplicationTests.java
+++ advanced/advanced/src/test/java/hello/advanced/AdvancedApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package hello.advanced;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AdvancedApplicationTests {
 
     @Test
     void contextLoads() {
