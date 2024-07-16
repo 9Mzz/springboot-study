5d123f990842553ade1a8b9ce4f0082842eaa8a4
05016 <nico1a@naver.com>
2024-07-16 20:24:37 +0900

    ThreadLocal - 예제 코드

---
+++ advanced/advanced/src/test/java/hello/advanced/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.threadlocal;
+
+import hello.advanced.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("tread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        // sleep(2000);    // 동시성 문제 발생 X
+        sleep(100); // 동시성 문제 발생 O/
+        threadB.start();
+        sleep(2000);    // 메인 Thread 종료 대기
+        log.info("main exit");
+
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
