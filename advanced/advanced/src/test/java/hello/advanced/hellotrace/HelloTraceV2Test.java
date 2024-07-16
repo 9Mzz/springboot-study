d6fbf20cf14fc586117db35ea06f14c7595e2402
05016 <nico1a@naver.com>
2024-07-16 10:39:55 +0900

    로그 추적기 V2 - 파라미터로 동기화 개발

---
+++ advanced/advanced/src/test/java/hello/advanced/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,27 @@
+package hello.advanced.hellotrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV2Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 traceV2 = new HelloTraceV2();
+        TraceStatus  status1 = traceV2.begin("hello 1");
+        TraceStatus  status2 = traceV2.beginSync(status1.getTraceId(), "hello 2");
+        traceV2.end(status2);
+        traceV2.end(status1);
+
+
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV2 traceV2 = new HelloTraceV2();
+        TraceStatus  status1 = traceV2.begin("hello 1");
+        TraceStatus  status2 = traceV2.beginSync(status1.getTraceId(), "hello 2");
+        traceV2.exception(status2, new IllegalStateException());
+        traceV2.exception(status2, new IllegalStateException());
+    }
+}
