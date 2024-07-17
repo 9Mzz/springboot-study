4939f98a006083ef7ba0c3acfb2646c2647c0ac8
05016 <nico1a@naver.com>
2024-07-17 19:45:01 +0900

    전략 패턴 - 예제3

---
+++ advanced/advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,52 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.ContextV2;
+import hello.advanced.trace.strategy.code.Strategy;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 적용 -> 익명 내부 Class
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        });
+        contextV2.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 적용 -> Ramda
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(() -> log.info("비즈니스 로직 1 실행"));
+        contextV2.execute(() -> log.info("비즈니스 로직 2 실행"));
+    }
+}
