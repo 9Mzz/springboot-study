311e4594d9af09b8c0667eb390855ad0ac10fb2a
05016 <nico1a@naver.com>
2024-07-19 12:05:02 +0900

    예제 프로젝트 만들기 v2

---
+++ advanced/proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+// @RequestMapping -> 스프링 부트 3.0 인식 불가
+@RestController
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        log.info("itemId: {}", itemId);
+        orderService.orderItem(itemId);
+        return itemId;
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
