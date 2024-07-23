2c71f377b619fd6059b2ea81a7d2ab46f33a205e
05016 <nico1a@naver.com>
2024-07-23 14:29:17 +0900

    빈 후처리기 - 예제 코드2

---
+++ advanced/proxy/src/test/java/hello/proxy/postprocessor/BasicPostProcessorTest.java
@@ -0,0 +1,77 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+public class BasicPostProcessorTest {
+
+    @Test
+    void basicConfig() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        // A는 bean 으로 등록된다
+/*         A beanA = applicationContext.getBean("beanA", A.class);
+        beanA.helloA(); */
+
+        // beanA 이름으로 B 객체가 Bean 으로 등록된다. A는 Bean 으로 등록된다.
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
+
+        // B는 bean 으로 등록되지 않는다
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean("beanB", B.class));
+
+
+    }
+
+    @Slf4j
+    @Configuration
+    static class BeanPostProcessorConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBProcessor helloProcessor() {
+            return new AToBProcessor();
+        }
+    }
+
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+
+    @Slf4j
+    static class AToBProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("bean = {}, beanName = {}", bean, beanName);
+            if (bean instanceof A) {
+                return new B();
+            }
+            return bean;
+        }
+    }
+}
