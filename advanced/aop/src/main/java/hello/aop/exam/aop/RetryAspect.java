3e423c2fdd9081616fa172f8e0b63fcf03bdcfc2
05016 <nico1a@naver.com>
2024-07-26 20:45:20 +0900

    프록시와 내부 호출 - 문제

--- advanced/aop/src/main/java/hello/aop/exam/aop/RetryAspect.java
+++ advanced/aop/src/main/java/hello/aop/exam/aop/RetryAspect.java
@@ -1,6 +1,5 @@
 package hello.aop.exam.aop;
 
-import hello.aop.exam.ExamService;
 import hello.aop.exam.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
