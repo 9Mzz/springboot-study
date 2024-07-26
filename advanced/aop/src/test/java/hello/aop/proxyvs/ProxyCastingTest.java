ba10ade163e6d0a2230e41afa4f88fc5e03914d0
05016 <nico1a@naver.com>
2024-07-26 21:25:27 +0900

    프록시 기술과 한계 - 타입 캐스팅

---
+++ advanced/aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,44 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target  = new MemberServiceImpl();
+        ProxyFactory      factory = new ProxyFactory(target);
+        // JDK 동적 프록시로 변경
+        factory.setProxyTargetClass(false);
+
+        // 프록시를 인터페이스로 캐스팅 -> 성공
+        MemberService memberServiceProxy = (MemberService) factory.getProxy();
+
+        // JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패, ClassCastException 예외 발생
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target  = new MemberServiceImpl();
+        ProxyFactory      factory = new ProxyFactory(target);
+        // JDK 동적 프록시로 변경
+        factory.setProxyTargetClass(true);
+
+        // 프록시를 인터페이스로 캐스팅 -> 성공
+        MemberService memberServiceProxy = (MemberService) factory.getProxy();
+
+        // CGLIB 동적 프록시를 구현 클래스로 캐스팅 시도 성공,
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+
+    }
+
+}
