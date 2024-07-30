249ab3b9555ae4b196387beb0cdfe0f6617840d6
05016 <nico1a@naver.com>
2024-07-30 16:05:32 +0900

    외부설정 사용 - @ConfigurationProperties 검증

--- principles_core/external-read/src/main/java/hello/config/MyDataSourceConfigV2.java
+++ principles_core/external-read/src/main/java/hello/config/MyDataSourceConfigV2.java
@@ -2,11 +2,9 @@ package hello.config;
 
 import hello.datasource.MyDataSource;
 import hello.datasource.MyDataSourcePropertiesV2;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-@Slf4j
 @EnableConfigurationProperties({MyDataSourcePropertiesV2.class})
 public class MyDataSourceConfigV2 {
 
