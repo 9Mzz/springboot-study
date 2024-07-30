c92cb7299ce0edaaf6b553c5f6b3b61762eae4f9
05016 <nico1a@naver.com>
2024-07-30 11:23:35 +0900

    외부 설정 사용 - Environment

--- principles_core/external-read/src/main/java/hello/ExternalReadApplication.java
+++ principles_core/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,12 @@
 package hello;
 
+import hello.config.MyDataSourceEnviConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceEnviConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
