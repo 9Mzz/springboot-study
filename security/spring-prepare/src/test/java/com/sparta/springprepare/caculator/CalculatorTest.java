98a4280c86eb6b777a71b578acb454d8e58fb94d
05016 <nico1a@naver.com>
2024-08-12 19:48:51 +0900

    application.properties 와 Lombok

---
+++ security/spring-prepare/src/test/java/com/sparta/springprepare/caculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.sparta.springprepare.caculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("더하기 테스트")
+    void test1() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(0, "+", 5);
+        System.out.println("result = " + result);
+        Assertions.assertThat(result)
+                  .isEqualTo(5);
+    }
+
+
+    @Test
+    @DisplayName("나누기 테스트")
+    void test2() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "/", 2);
+        System.out.println("result = " + result);
+        Assertions.assertThat(result)
+                  .isEqualTo(4);
+    }
+}
