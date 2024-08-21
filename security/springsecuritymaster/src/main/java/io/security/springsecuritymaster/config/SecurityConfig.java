97bfaaa49535cbbbc52a7afd4ec3728e830e2e94
05016 <nico1a@naver.com>
2024-08-21 21:05:56 +0900

    사용자 정의 보안 설정하기

--- security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
+++ security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package io.security.springsecuritymaster.config;
 
 
+import org.apache.catalina.realm.UserDatabaseRealm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,52 +13,37 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest()
-                                               .authenticated())
-            //formLogin()
-            .formLogin(form -> form
-//                    .loginPage("/loginPage")
-.loginProcessingUrl("/loginProc")
-.defaultSuccessUrl("/", false)
-.failureUrl("/failed")
-.usernameParameter("userId")
-.passwordParameter("passwd")
-.successHandler((request, response, authentication) -> {
-    System.out.println("authentication : " + authentication);
-    response.sendRedirect("/home");
-})
-.failureHandler((request, response, exception) -> {
-    System.out.println("exception : " + exception.getMessage());
-    response.sendRedirect("/login");
-})
-.permitAll());
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest()
+                        .permitAll())
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
+
         UserDetails userA = User.withUsername("userA")
-                                .password("{noop}1234")
-                                .roles("USER")
-                                .build();
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
 
         UserDetails userB = User.withUsername("userB")
-                                .password("{noop}1234")
-                                .roles("USER")
-                                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                                .password("{noop}1234")
-                                .roles("ADMIN")
-                                .build();
-        return new InMemoryUserDetailsManager(userA, userB, admin);
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+        
+        return new InMemoryUserDetailsManager(userA, userB);
     }
+
 
 }
