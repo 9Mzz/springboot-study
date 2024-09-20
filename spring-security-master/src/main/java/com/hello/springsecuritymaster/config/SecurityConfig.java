3fd05d1a760ea7116cbcbf05b392adb60e8bb2e4
BackPicker <nico1a@naver.com>
2024-09-20 15:08:52 +0900

    SecurityContextRepository / SecurityContextHolderFilter

--- spring-security-master/src/main/java/com/hello/springsecuritymaster/config/SecurityConfig.java
+++ spring-security-master/src/main/java/com/hello/springsecuritymaster/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.hello.springsecuritymaster.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,19 +20,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // SecurityContext 를 명시적으로 저장할 것이지 아닌지의 여부 설정, 기본값은 true 입니다
-        // true 이면 SecurityContextHolderFilter, false 이면 SecurityContextPersistanceFilter 가 실행됩니다
-        http.securityContext(context -> context.requireExplicitSave(true));
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = builder.build();
 
-
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(Customizer.withDefaults())
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return customAuthenticationFilter;
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userA = User.withUsername("userA").password("{noop}1234").roles("USER").build();
+        UserDetails userA = User.withUsername("userA")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
         return new InMemoryUserDetailsManager(userA);
     }
 
--- spring-security-master/src/main/java/com/hello/springsecuritymaster/controller/IndexController.java
+++ spring-security-master/src/main/java/com/hello/springsecuritymaster/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.hello.springsecuritymaster.controller;
 
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public Authentication index(Authentication authentication) {
+        return authentication;
     }
 
     @GetMapping("/home")
