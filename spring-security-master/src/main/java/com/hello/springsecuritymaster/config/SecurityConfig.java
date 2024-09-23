f0bb8001a11192293a4d72a1acaf0643e1f074c8
BackPicker <nico1a@naver.com>
2024-09-23 15:58:25 +0900

    동시 세션 제어 - sessionManagement().maximumSessions()

--- spring-security-master/src/main/java/com/hello/springsecuritymaster/config/SecurityConfig.java
+++ spring-security-master/src/main/java/com/hello/springsecuritymaster/config/SecurityConfig.java
@@ -2,11 +2,9 @@ package com.hello.springsecuritymaster.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,21 +17,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/login")
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/login")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-        // .formLogin(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .invalidSessionUrl("/invalidSessionUrl")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false)
+                        .expiredUrl("/expired")
+                )
+
         ;
 
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
