7ae89bbe65a9d9a08fa529ec4b35d4b336142fe1
BackPicker <nico1a@naver.com>
2024-09-24 00:31:47 +0900

    세션 생성 정책 - sessionManagement().sessionCreationPolicy()

--- spring-security-master/src/main/java/com/hello/springsecuritymaster/config/SecurityConfig.java
+++ spring-security-master/src/main/java/com/hello/springsecuritymaster/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,35 +18,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated());
 
         http.formLogin(Customizer.withDefaults());
-        // false 인 경우
-        // invalidSessionUrl X expiredUrl X -> This session has been expired
-        // invalidSessionUrl O expiredUrl X -> This session has been expired
-        // invalidSessionUrl X expiredUrl O -> expiredUrl()에 설정된 URL로 리다이렉션
-        // invalidSessionUrl O expiredUrl O -> invalidSessionUrl()에 설정된 URL로 리다이렉션
-        // true 인 경우 -> 인증 차단
 
-        // 추천 -> maxSessionsPreventsLogin(false), expiredUrl 만 사용
-        http.sessionManagement(session -> session
-                // fix.changeSessionId() -> 기존 세션을 유지하면서 세션 ID만 변경하여 인증 과정에서 세션 고정 공격을 방지하는 방식이다. 기본 값으로 설정되어 있다
-                // fix.newSession() -> 새로운 세션을 생성하고 기존 세션 데이터를 복사하지 않는 방식이다(SPRING_SECURITY_ 로 시작하는 속성은 복사한다)
-                // fix.migrateSession() -> 새로운 세션을 생성하고 모든 기존 세션 속성을 새 세션으로 복사한다
-                // fix.none() -> 기존 세션을 그대로 사용한다
-                .sessionFixation(fix -> fix.none())
-                // 이미 만료된 세션으로 요청을 하는 사용자를 특정 엔드포인트로 리다이렉션 할 Url 을 지정한다
-                .invalidSessionUrl("/invalidSession")
-                // 사용자당 최대 세션 수를 제어한다. 기본값은 무제한 세션을 허용한다
-                .maximumSessions(1)
-                // true 이면 최대 세션 수(maximumSessions(int))에 도달했을 때 사용자의 인증을 방지한다
-                // false(기본 설정)이면 인증하는 사용자에게 접근을 허용하고 기존 사용자의 세션은 만료된다
-                .maxSessionsPreventsLogin(false)
-                // 세션을 만료하고 나서 리다이렉션 할 URL 을 지정한다
-                .expiredUrl("/expired"));
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+
+
         return http.build();
     }
 
